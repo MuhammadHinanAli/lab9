b77c9a5aa0d440a8a2dbfb6d7e22c1b830f62b36
Muhammad Hinan Ali <g00409830@atu.ie>
2023-04-20 14:36:42 +0000

    lab9 complete

--- src/app/status/status.page.ts
+++ src/app/status/status.page.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Storage} from '@ionic/storage-angular'; //import
+//import { NavController} from '@ionic/angular'; //import for navigating
 @Component({
   selector: 'app-status',
   templateUrl: './status.page.html',
@@ -8,11 +9,12 @@ import {Storage} from '@ionic/storage-angular'; //import
 export class StatusPage implements OnInit {
   myStatus:string =""; //varuable to store the status such as sad , happy.
 
-  constructor(private storage:Storage) { }
+  constructor(private storage:Storage){ } //private navCtrl:NavController //navigating
 
   async onSave(){  //method with async
     await this.storage.create();  //built in function //created a databse
-    await this.storage.set("status", this.myStatus); //built in function //will not run till the first prompt runs// set it
+    await this.storage.set("status", this.myStatus); //built in function //will not run till the first prompt runs// set it adn pass the key
+    //await this.navCtrl.navigateBack('/home'); //to navigate after the second prompt to the home page
   }
 
   ngOnInit() {
